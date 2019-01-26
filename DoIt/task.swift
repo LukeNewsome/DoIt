7ba6df4dc4017241cc8c8ef40466888c433f29b7
Luke Newsome <luke@newsome.org>
2019-01-26 14:27:40 -0600

    First Screen

---
+++ DoIt/task.swift
@@ -0,0 +1,17 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Luke Newsome on 1/26/19.
+//  Copyright © 2019 Zappy Code. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+
+    
+}
