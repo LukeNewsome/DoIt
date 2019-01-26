5e5486baaa8a2313ea6477552e2462decb418f28
Luke Newsome <luke@newsome.org>
2019-01-26 16:53:19 -0600

    set switch and add up

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by Luke Newsome on 1/26/19.
+//  Copyright © 2019 Zappy Code. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addTapped(_ sender: Any) {
+        
+    }
+    
+
+    
+ 
+
+}
