ff78f3ff3b030958c8df458233d7c26c406e0514
Luke Newsome <luke@newsome.org>
2019-01-27 11:13:19 -0600

    Finished all but core data

--- DoIt/TasksViewController.swift
+++ DoIt/TasksViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 
 class TasksViewController: UIViewController,UITableViewDelegate, UITableViewDataSource{
-
+    
     @IBOutlet weak var tableView: UITableView!
     var tasks: [Task] = []
+    var selectedIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,10 +22,10 @@ class TasksViewController: UIViewController,UITableViewDelegate, UITableViewData
         tableView.dataSource = self
         tableView.delegate = self
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
-       
+        
         
     }
     
@@ -34,24 +35,37 @@ class TasksViewController: UIViewController,UITableViewDelegate, UITableViewData
         if task.important {
             cell.textLabel?.text = "❗️\(task.name)"
         } else {
-    cell.textLabel?.text = task.name}
+            cell.textLabel?.text = task.name}
         
         return cell
-}
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedIndex = indexPath.row
+        let task = tasks[indexPath.row]
+        
+        performSegue(withIdentifier: "selectTaskSegue", sender: task)
+    }
     
     @IBAction func plusTapped(_ sender: Any) {
         performSegue(withIdentifier: "addSegue", sender: nil)
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "addSegue" {
         let nextVC = segue.destination as! CreateTaskViewController
-        nextVC.previousVC = self
-    }
+            nextVC.previousVC = self}
+        
+        if segue.identifier == "selectTaskSegue" {
+        let nextVC = segue.destination as! CompleteTaskViewController
+            nextVC.task = sender as! Task
+            nextVC.previousVC = self
+        }
+        
 }
 func makeTasks() -> [Task] {
     let task1 = Task()
     task1.name = "Walk the dog"
-task1.important = false
-
+    task1.important = false
+    
     let task2 = Task()
     task2.name = "Buy Cheese"
     task2.important = true
@@ -59,11 +73,13 @@ task1.important = false
     let task3 = Task()
     task3.name = "Mow the Lawn"
     task3.important = false
-
-return [task1, task2, task3]
-
-
-
-
-
+    
+    return [task1, task2, task3]
+    
+    
+    
+    
+    
+    
+}
 }
